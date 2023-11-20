3208a8a4c96b1cafcbb4ad241927e9091f05d53f
Xaverius <kevin.kevin@weekendinc.com>
2023-11-20 23:12:49 +0700

    fix : add TrustedSender for security

---
+++ lib/multihop/TrustedSender.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+// TrustedSender Contracts v0.0.1
+// Creator: Nava Labs
+
+pragma solidity ^0.8.19;
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {CCIPDirectory} from "./CCIPDirectory.sol";
+
+contract TrustedSender is Ownable, CCIPDirectory {
+
+    /**
+     * @dev Emitted when Trusted Sender is set
+     */
+    event SetTrustedSender(uint64[] _senderChainId, address[] _senderAddress);
+
+    /**
+     * Returns the trusted sender in specific chain
+     */
+    function getTrustedSender(uint64 _senderChainId) external view returns (address) {
+       CrossChainMetadataAddress memory _metadataChainSender = getConfigFromNetwork(_senderChainId);
+       return _metadataChainSender.crossChainApp;
+    }
+
+    function updateCrossChainApp(uint64[] memory chainSelector, address[] memory crossChainAppAddress) external override onlyOwner {
+       for (uint256 i = 0; i < chainSelector.length; i++) {
+            _crossChainMetadataAddress[chainSelector[i]].crossChainApp = crossChainAppAddress[i];
+        }
+
+        emit SetTrustedSender(chainSelector,crossChainAppAddress);
+    }
+
+    function isTrustedSender(uint64 _senderChainId , address senderMessage) internal view returns (bool) {
+       CrossChainMetadataAddress memory _metadataChainSender = getConfigFromNetwork(_senderChainId);
+       return _metadataChainSender.crossChainApp == senderMessage;
+    }
+
+ }
