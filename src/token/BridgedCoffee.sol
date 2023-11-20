ec507ad26f03350bd1af3c761e585b9dc6d82ee0
Xaverius <kevin.kevin@weekendinc.com>
2023-11-20 23:14:46 +0700

    add mock token for testing that inherit proxy

---
+++ src/token/BridgedCoffee.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import {TokenProxy_Destination , ERC20} from "../proxy/TokenProxy_Destination.sol";
+
+contract BridgedCoffee is ERC20, TokenProxy_Destination {
+    constructor(
+        string memory name, 
+        string memory symbol, 
+        address router,
+        uint64 chainIdThis
+    ) ERC20(name, symbol) TokenProxy_Destination(router,chainIdThis) {}
+}
