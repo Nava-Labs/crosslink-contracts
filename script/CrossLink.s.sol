f160fd9db6837d3050c415640f2eb4d9fc378bd9
rwin <erwinphanglius10@gmail.com>
2023-11-23 18:07:43 +0700

    add script for withdraw link

--- script/CrossLink.s.sol
+++ script/CrossLink.s.sol
@@ -13,7 +13,7 @@ contract DeployCrossLinkSepolia is Script, CCIPHelper {
         vm.startBroadcast(deployerPrivateKey);
 
         (address router, , , ) = getConfigFromNetwork(chain);
-        address _tokenPayment = 0xA364E5A4D3F7Bf47Bcaa135634dFe1e47B2c57b8;
+        address _tokenPayment = 0xeB21Ad3953eDB8228a50E4f0FcD6A1F6391b38e7;
 
         uint64 chainIdThis = 16015286601757825753;
         uint64 chainIdMaster = 16015286601757825753;
@@ -40,7 +40,7 @@ contract DeployCrossLinkOpGoerli is Script, CCIPHelper {
         vm.startBroadcast(deployerPrivateKey);
 
         (address router, , , ) = getConfigFromNetwork(chain);
-        address _tokenPayment = 0x7794DECEb421974aD5f61Cd04699715CAeb10638;
+        address _tokenPayment = 0x84Ca5978286e5c49152424d68101B07465a89D53;
 
         uint64 chainIdThis = 2664363617261496610;
         uint64 chainIdMaster = 16015286601757825753;
@@ -67,7 +67,7 @@ contract DeployCrossLinkFuji is Script, CCIPHelper {
         vm.startBroadcast(deployerPrivateKey);
 
         (address router, , , ) = getConfigFromNetwork(chain);
-                address _tokenPayment = 0x48156d3EA56bb4F120b1a942Cea061AbbA9fb989;
+                address _tokenPayment = 0x6B13990cf43D2Abfe785AB7De89067653BACbeF3;
 
         uint64 chainIdThis = 14767482510784806043;
         uint64 chainIdMaster = 16015286601757825753;
@@ -150,7 +150,7 @@ contract DeployCrossLinkBaseGoerli is Script, CCIPHelper {
         vm.startBroadcast(deployerPrivateKey);
 
         (address router, , , ) = getConfigFromNetwork(chain);
-        address _tokenPayment = address(0);
+        address _tokenPayment = 0x4C4b43A82fb9222E8505FBf6030D1F6D92a6ff9C;
         
         uint64 chainIdThis = 5790810961207155433;
         uint64 chainIdMaster = 16015286601757825753;
@@ -177,7 +177,7 @@ contract DeployCrossLinkBscTestnet is Script, CCIPHelper {
         vm.startBroadcast(deployerPrivateKey);
 
         (address router, , , ) = getConfigFromNetwork(chain);
-        address _tokenPayment = 0x7794DECEb421974aD5f61Cd04699715CAeb10638;
+        address _tokenPayment = 0xa23C69E42a8CBf7270cFAE05B6079A07B47C5612;
         
         uint64 chainIdThis = 13264668187771770619;
         uint64 chainIdMaster = 16015286601757825753;
@@ -224,6 +224,19 @@ contract DistributeLink is Script, CCIPHelper {
     }
 }
 
+contract WithdrawLink is Script, CCIPHelper {
+    function run(SupportedNetworks network, address payable to, address beneficiary) external {
+        (, address link, , ) = getConfigFromNetwork(network);
+
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        CrossLinkMarketplace(to).withdrawToken(beneficiary, link);
+
+        vm.stopBroadcast();
+    }
+}
+
 contract Marketplace is Script, CCIPHelper {
     function listing(address payable to, address tokenAddress, uint256 tokenId, uint256 price) external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
@@ -244,3 +257,4 @@ contract Marketplace is Script, CCIPHelper {
     }
 
 }
+
