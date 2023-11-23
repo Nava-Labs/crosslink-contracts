e26a0987e69f4fe5012d860fdd9e499fe4a8fa36
rwin <erwinphanglius10@gmail.com>
2023-11-24 03:54:53 +0700

    fix: delete unused test

--- test/CrossLink.t.sol
+++ test/CrossLink.t.sol
@@ -2,23 +2,6 @@
 pragma solidity ^0.8.13;
 
 import {Test, console2} from "forge-std/Test.sol";
-import {CrossLink} from "../src/CrossLink.sol";
+import {CrossLinkMarketplace} from "../src/marketplace/CrossLinkMarketplace.sol";
 
-contract CrossLinkTest is Test {
-    CrossLink public counter;
-
-    function setUp() public {
-        counter = new CrossLink();
-        counter.setNumber(0);
-    }
-
-    function test_Increment() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
-    }
-
-    function testFuzz_SetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
-    }
-}
+contract CrossLinkMarketplaceTest is Test { }
